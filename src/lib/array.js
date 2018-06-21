7425d97d81fb5ce8233ad1d6cd2c6c1aa5fcdb70
Carlos Villuendas <carlosvillu@gmail.com>
2018-06-21 19:40:52 +0200

    feat(lib): add array and function helpers

---
+++ src/lib/array.js
@@ -0,0 +1,10 @@
+export const pickRnd = arr => arr[Math.floor(Math.random() * arr.length)]
+
+export const shuffle = ([...arr]) => {
+  let m = arr.length
+  while (m) {
+    const i = Math.floor(Math.random() * m--)
+    ;[arr[m], arr[i]] = [arr[i], arr[m]]
+  }
+  return arr
+}
---
+++ src/lib/func.js
@@ -0,0 +1,2 @@
+const _pipe = (a, b) => arg => b(a(arg))
+export const pipe = (...ops) => ops.reduce(_pipe)
