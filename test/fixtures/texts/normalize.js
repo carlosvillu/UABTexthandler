62c6333542ba678be40c36597cc9ce2326e5b86c
Carlos Villuendas <carlosvillu@gmail.com>
2018-10-06 18:44:09 +0200

    test(META): normalize texts use case

---
+++ test/fixtures/texts/normalize.js
@@ -0,0 +1,36 @@
+export const original = `
+Fer bons textos@o d'opinió@o amb@o totes@o les@o seves@o parts [% punt AP] .
+Jo crec .
+que sí@o, .
+perquè@o los@s nens juguin@o un poc@o [% punt AP] .
+La primera raó és@o .
+que los@s nens se@s cansarán@s .
+de tanto@s treballar@o.
+También@s tienen@s que@s jugar@s una estonerita .
+per@o divertir-se@o [% punt AP] .
+La segona raó, también@s si@o salen@s al pati, .
+no se@s avorriran@o molt.
+También@s querrán@s jugar@s al@s futbol, etc [% punt AP] .
+La tercera@o raón@s, también@s pueden@s tener@s ganas@s de .
+jugar@s, .
+i también@s querrán@s jugar@s al pati .
+porque@s estarán@s molt cansat@o [% punt AP] .
+La meva conclusió@o és@o .
+que los@s nen se@s aburrirán@o d' .
+estar treballant@o .
+i querrán@s jugar@s al pati.
+`.trim()
+
+export const normalize = `
+Fer bons textos d'opinió amb totes les seves parts.
+
+Jo crec que sí, perquè los nens juguin un poc.
+
+La primera raó és que los nens se cansarán de tanto treballar También tienen que jugar una estonerita per divertir-se.
+
+La segona raó, también si salen al pati, no se avorriran molt También querrán jugar al futbol, etc.
+
+La tercera raón, también pueden tener ganas de jugar, i también querrán jugar al pati porque estarán molt cansat.
+
+La meva conclusió és que los nen se aburrirán d' estar treballant i querrán jugar al pati.
+`.trim()
---
+++ test/texts/NormalizeTextsUseCaseSpec.js
@@ -0,0 +1,102 @@
+import {original, normalize as result} from '../fixtures/texts/normalize'
+import {expect} from 'chai'
+import Domain from '../../src/domain'
+
+let domain
+describe('Normalize rules', () => {
+  beforeEach(() => {
+    domain = new Domain()
+  })
+
+  it('Should remove all @o ocurrencies', async () => {
+    const input = 'perquè@o los@s nens juguin@o un poc@o [% punt AP] .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    return expect(normalized.indexOf('@o')).to.equal(-1)
+  })
+
+  it('Should remove all @s ocurrencies', async () => {
+    const input = 'perquè@o los@s nens juguin@o un poc@o [% punt AP] .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized.indexOf('@s')).to.equal(-1)
+  })
+
+  it('Should remove all @s:[LANGUAGE] ocurrencies', async () => {
+    const input = 'perquè@o los@s:CAT nens juguin@s:ESP un poc@o [% punt AP] .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized.indexOf(':CATALAN')).to.equal(-1)
+    expect(normalized.indexOf(':ESPAÑOL')).to.equal(-1)
+  })
+
+  it('Should remove all @o@s ocurrencies', async () => {
+    const input =
+      'perquè@o@s los@s:CAT nens juguin@s:ESP un poc@o@s [% punt AP] .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized.indexOf(':CATALAN')).to.equal(-1)
+    expect(normalized.indexOf(':ESPAÑOL')).to.equal(-1)
+    expect(normalized.indexOf('@o')).to.equal(-1)
+    expect(normalized.indexOf('@s')).to.equal(-1)
+    expect(normalized.indexOf('@o@s')).to.equal(-1)
+  })
+
+  it('Should replace all [% interrogació] ocurrencies with "? "', async () => {
+    const input = 'perquè los nens [% interrogació] juguin un poc .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized).to.be.eql('perquè los nens ? juguin un poc .')
+  })
+
+  it('Should replace all [% exclamació] ocurrencies with "! "', async () => {
+    const input =
+      'perquè los nens [% exclamació] juguin [% exclamació] un poc .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized).to.be.eql('perquè los nens ! juguin ! un poc .')
+  })
+
+  it('Should replace all [% suspensius] ocurrencies with "... "', async () => {
+    const input =
+      'perquè los nens [% suspensius] juguin [% suspensius] un poc .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized).to.be.eql('perquè los nens ... juguin ... un poc .')
+  })
+
+  it('Should replace all [% punt AP] ocurrencies with ".\\n\\n"', async () => {
+    const input = 'perquè los nens [% punt AP] juguin [% punt AP] un poc .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized).to.be.eql('perquè los nens.\n\njuguin.\n\nun poc .')
+  })
+
+  it('Should replace all [% AP] ocurrencies with "\\n\\n"', async () => {
+    const input = 'perquè los nens [% AP] juguin [% AP] un poc .'
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: input})
+    expect(normalized).to.be.eql('perquè los nens\n\njuguin\n\nun poc .')
+  })
+})
+describe('Normalize texts', () => {
+  beforeEach(() => {
+    domain = new Domain()
+  })
+
+  it('Should have a normalized text with all rules apply', async () => {
+    const normalized = await domain
+      .get('normalize_texts_use_case')
+      .execute({text: original})
+    expect(normalized).to.be.eql(result)
+  })
+})
