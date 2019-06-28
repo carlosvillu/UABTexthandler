693171758b6111902dfc007c10eb5c94aaaf47aa
Carlos Villuendas <carlosvillu@gmail.com>
2019-06-29 00:19:30 +0200

    feat(components): add NumberSelector component

--- src/components/QualityQuiz/index.js
+++ src/components/QualityQuiz/index.js
@@ -7,28 +7,40 @@ import withReducer from 'recompose/withReducer'
 import withHandlers from 'recompose/withHandlers'
 
 export default compose(
-  getContext({i18n: PropTypes.object}),
+  getContext({domain: PropTypes.object, i18n: PropTypes.object}),
   withReducer(
-    'quality',
+    'form',
     'dispatch',
     (state, action) => {
       switch (action.type) {
         case 'change-grade':
           return {
             ...state,
-            grade: action.grade
+            grade: action.grade,
+            quality: null
+          }
+        case 'change-quality':
+          return {
+            ...state,
+            quality: action.quality
           }
 
         default:
           return state
       }
     },
-    props => ({grade: null})
+    props => ({grade: null, quality: null})
   ),
   withHandlers({
     handleChangeSelect: props => (evt, indexOption, value) => {
-      props.dispatch({type: 'change-grade', grade: value}, state =>
+      props.dispatch({type: 'change-grade', grade: value}, state => {
         props.onChangeGrade(state.grade)
+        props.onChangeQuality(state.quality)
+      })
+    },
+    handleChangeQuality: props => quality => {
+      props.dispatch({type: 'change-quality', quality}, state =>
+        props.onChangeQuality(state.quality)
       )
     }
   })
