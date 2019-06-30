2301b65cc48d87a07678c67145165e041f67b731
Carlos Villuendas <carlosvillu@gmail.com>
2019-06-30 12:08:58 +0200

    feat(pages): add QualityTable to the page

--- src/pages/AdminEvaluations/component.js
+++ src/pages/AdminEvaluations/component.js
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types'
 
 import Downloader from '../../components/Downloader'
 import TableStructureEvaluations from '../../components/TableStructureEvaluations'
+import TableQualityEvaluations from '../../components/TableQualityEvaluations'
 
 const AdminEvaluations = ({i18n}) => (
   <div className="AdminEvaluations">
-    <TableStructureEvaluations />
+    <TableStructureEvaluations
+      tip={false}
+      title={i18n.t('ADMIN_EVALUATIONS_STUCTURE_TITLE')}
+    />
+    <TableQualityEvaluations
+      title={i18n.t('ADMIN_EVALUATIONS_QUALITY_TITLE')}
+    />
     <Downloader />
   </div>
 )
