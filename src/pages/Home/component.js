94652c2087c7fa66dd8d5cfd866f240185f3bafb
Carlos Villuendas <carlosvillu@gmail.com>
2019-06-30 17:53:10 +0200

    feat(pages): home page with two buttons

--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,82 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 
-import RaisedButton from 'material-ui/RaisedButton'
-import FlatButton from 'material-ui/FlatButton'
+import Link from 'react-router/lib/Link'
 
-import CanvasTexto from '../../components/CanvasTexto'
-import StructureQuiz from '../../components/StructureQuiz'
-
-class Home extends React.PureComponent {
-  static TEXT_TAB = 'text'
-  static QUESTIONS_TAB = 'questions'
-  static propTypes = {
-    domain: PropTypes.object,
-    handleChangeQuiz: PropTypes.func,
-    handleClickFlatButton: PropTypes.func,
-    handleClickRaisedButton: PropTypes.func,
-    handleInitQuiz: PropTypes.func,
-    i18n: PropTypes.object,
-    setStateText: PropTypes.func,
-    stateText: PropTypes.object
-  }
-
-  async componentDidMount() {
-    const user = await this.props.domain.get('current_users_use_case').execute()
-    const text = await this.props.domain
-      .get('get_next_evaluation_texts_use_case')
-      .execute({user, type: 'structure'})
-    this.props.setStateText(text)
-  }
-
-  render() {
-    const {
-      handleChangeQuiz,
-      handleClickFlatButton,
-      handleClickRaisedButton,
-      handleInitQuiz,
-      i18n,
-      stateText
-    } = this.props
-
-    return (
-      <div className="Home">
-        <Helmet />
-        <div className="Home-Body">
-          <div className="Home-BodyCanvas">
-            {stateText ? (
-              <CanvasTexto student={stateText.student}>
-                {stateText.normalize}
-              </CanvasTexto>
-            ) : (
-              <h2>{i18n.t('HOME_LOOKING_TEXTS')}</h2>
-            )}
-          </div>
-          <div className="Home-BodyQuiz">
-            <StructureQuiz
-              onInit={handleInitQuiz}
-              onChange={handleChangeQuiz}
-            />
-            {stateText && (
-              <div className="Home-buttons">
-                <FlatButton
-                  label="Saltar texto"
-                  onClick={handleClickFlatButton}
-                />
-                <RaisedButton
-                  label="Guardar y seguir"
-                  onClick={handleClickRaisedButton}
-                  primary
-                  style={{margin: 12}}
-                />
-              </div>
-            )}
-          </div>
-        </div>
+const Home = ({i18n}) => (
+  <div className="Home">
+    <div className="Home-Buttons">
+      <div className="Home-Button">
+        <Link to="/quality" className="Home-Link">
+          {i18n.t('HOME_BUTTON_QUALITY')}
+        </Link>
       </div>
-    )
-  }
+      <div className="Home-Button">
+        <Link to="/structure" className="Home-Link">
+          {i18n.t('HOME_BUTTON_STRUCTURE')}
+        </Link>
+      </div>
+    </div>
+  </div>
+)
+
+Home.propTypes = {
+  i18n: PropTypes.object
 }
 
 export default Home
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,38 +3,5 @@ import PropTypes from 'prop-types'
 
 import compose from 'recompose/compose'
 import getContext from 'recompose/getContext'
-import withState from 'recompose/withState'
-import withHandlers from 'recompose/withHandlers'
 
-export default compose(
-  withState('stateText', 'setStateText'),
-  withState('stateEvaluation', 'setStateEvaluation'),
-  withState('stateErrors', 'setStateErrors'),
-  getContext({domain: PropTypes.object, i18n: PropTypes.object}),
-  withHandlers({
-    handleInitQuiz: props => evaluation => props.setStateEvaluation(evaluation),
-    handleChangeQuiz: props => evaluation => {
-      console.log(evaluation) // eslint-disable-line
-      props.setStateEvaluation(evaluation)
-    },
-    handleClickFlatButton: props => evt => {
-      props.router.push('/')
-      window.scrollTo(0, 0)
-    },
-    handleClickRaisedButton: props => async evt => {
-      const {domain, stateText, stateEvaluation, i18n} = props
-      try {
-        const user = await domain.get('current_users_use_case').execute()
-        await domain
-          .get('save_structure_evaluation_texts_use_case')
-          .execute({user, evaluation: stateEvaluation, text: stateText})
-        props.router.push('/')
-        window.scrollTo(0, 0)
-      } catch (err) {
-        window.alert(
-          `${i18n.t('HOME_ALERT_ERROR_TITLE')}\n\n${err.errors().join(', ')}`
-        )
-      }
-    }
-  })
-)(Home)
+export default compose(getContext({i18n: PropTypes.object}))(Home)
