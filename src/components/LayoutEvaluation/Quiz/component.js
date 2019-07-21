87177f3656b1a24b00a601f303569956f0c1538b
Carlos Villuendas <carlosvillu@gmail.com>
2019-07-21 17:15:29 +0200

    feat(components): disabled buttons when skip or save evaluation

--- src/components/LayoutEvaluation/Quiz/component.js
+++ src/components/LayoutEvaluation/Quiz/component.js
@@ -4,19 +4,28 @@ import PropTypes from 'prop-types'
 import RaisedButton from 'material-ui/RaisedButton'
 import FlatButton from 'material-ui/FlatButton'
 
-const Quiz = ({children, text, i18n, onClickSkip, onClickSave}) => {
+const Quiz = ({
+  children,
+  handleClickSave,
+  handleClickSkip,
+  i18n,
+  stateDisableButtons,
+  text
+}) => {
   return (
     <div className="LayoutEvaluation-BodyQuiz">
       {children}
       {text && (
         <div className="LayoutEvaluation-buttons">
           <FlatButton
+            disabled={stateDisableButtons}
             label={i18n.t('LAYOUT_EVALUATION_QUIZ_BUTTON_SKIP')}
-            onClick={onClickSkip}
+            onClick={handleClickSkip}
           />
           <RaisedButton
+            disabled={stateDisableButtons}
             label={i18n.t('LAYOUT_EVALUATION_QUIZ_BUTTON_SAVE')}
-            onClick={onClickSave}
+            onClick={handleClickSave}
             primary
             style={{margin: 12}}
           />
@@ -29,8 +38,9 @@ const Quiz = ({children, text, i18n, onClickSkip, onClickSave}) => {
 Quiz.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element),
   i18n: PropTypes.object,
-  onClickSave: PropTypes.func,
-  onClickSkip: PropTypes.func,
+  handleClickSave: PropTypes.func,
+  handleClickSkip: PropTypes.func,
+  stateDisableButtons: PropTypes.bool,
   text: PropTypes.shape({
     student: PropTypes.string,
     normalize: PropTypes.string
--- src/components/LayoutEvaluation/Quiz/index.js
+++ src/components/LayoutEvaluation/Quiz/index.js
@@ -3,5 +3,22 @@ import PropTypes from 'prop-types'
 
 import compose from 'recompose/compose'
 import getContext from 'recompose/getContext'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
-export default compose(getContext({i18n: PropTypes.object}))(Quiz)
+export default compose(
+  withState('stateDisableButtons', 'setStateDisableButtons', false),
+  getContext({i18n: PropTypes.object}),
+  withHandlers({
+    handleClickSkip: props => async evt => {
+      props.setStateDisableButtons(true)
+      await props.onClickSkip()
+      props.setStateDisableButtons(false)
+    },
+    handleClickSave: props => async evt => {
+      props.setStateDisableButtons(true)
+      await props.onClickSave()
+      props.setStateDisableButtons(false)
+    }
+  })
+)(Quiz)
