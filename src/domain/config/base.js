edb51fd2729c35bc56e37b815941fdd5fdb1c8a2
Carlos Villuendas <carlosvillu@gmail.com>
2019-12-08 21:54:04 +0100

    feat(domain): add new grades

--- src/domain/config/base.js
+++ src/domain/config/base.js
@@ -2,7 +2,10 @@ export default {
   API_HOST: 'localhost:3001',
   GRADES: {
     SECOND_PRIMARY: 2,
+    THIRD_PRIMARY: 3,
     FOURTH_PRIMARY: 4,
+    FIFTH_PRIMARY: 5,
+    SIXTH_PRIMARY: 6,
     SECOND_ESO: 8
   },
   GENRE: {
