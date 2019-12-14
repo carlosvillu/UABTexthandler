58cbda4730f24022d6cfb2215c8572c5b2747eab
Carlos Villuendas <carlosvillu@gmail.com>
2019-12-14 14:09:37 +0100

    feat(domain): apply time to calculate level

--- src/domain/texts/Mappers/JSONTextToTextEntityTextsMapper.js
+++ src/domain/texts/Mappers/JSONTextToTextEntityTextsMapper.js
@@ -4,15 +4,20 @@ export default class JSONTextToTextEntityTextsMapper {
     this._normalizeMapper = normalizeMapper
   }
 
+  applyTimeToLevel({value, time}) {
+    const [_, delta = 0] = time.split(/SEG/)
+    return parseInt(value, 10) + parseInt(delta, 10)
+  }
+
   map = ({filename, body}) => {
     const [idFile] = filename.split('.')
     const [, school, level, room, student, time, gender] = filename.match(
-      /^(\d\d)(\d)(\d)(\d\d\d\d)(PRE|POST|MANT|S1|S2)-(OP|NA)/i
+      /^(\d\d)(\d)(\d)(\d\d\d\d)(PRE|POST|MANT|SEG1|SEG2)-(OP|NA)/i
     )
     return this._textEntityFactory({
       idText: parseInt(`${school}${level}${room}${student}`, 10),
       school: parseInt(school, 10),
-      level: parseInt(level, 10),
+      level: this.applyTimeToLevel({value: level, time}),
       room: parseInt(room, 10),
       student: parseInt(student, 10),
       original: body,
