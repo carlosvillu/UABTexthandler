aefd89359172f031e8416ef155c16849084f490d
Carlos Villuendas <carlosvillu@gmail.com>
2020-12-23 16:43:01 +0100

    chore(META): create a "stats" commando to show texts numbers info

---
+++ bin/super-texthandler-stats.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+/* eslint no-console:0 */
+
+const program = require('commander')
+
+program
+  .option('-D, --debug', 'Show extra metrics')
+  .option('-P, --path <path>', 'Where is the full DB JSON file')
+  .option('-T, --type <type>', 'Type os evaluation "structure" or "quality"')
+  .on('--help', () => {
+    console.log('  Description:')
+    console.log('')
+    console.log(
+      ' Run this command to get a table with stats about the current state of the evaluations in the platform'
+    )
+    console.log(
+      ' You will get Number of texts in the platform, number of texts with zero, one or two evaluations'
+    )
+    console.log('')
+    console.log('  Examples:')
+    console.log('')
+    console.log(
+      '    $ super-texthandler stats -P /path/to/backup_DB.json -T quality'
+    )
+    console.log('')
+  })
+  .parse(process.argv)
+
+const {type, path, debug} = program
+
+if (!['structure', 'quality'].includes(type)) {
+  console.error(`Type ${type} not allowed. Use "structure" or "quality"`)
+  process.exit(1)
+}
+
+const db = require(path)
+
+const texts = db.texts
+
+const stats = Object.values(texts).reduce((acc, text) => {
+  const id = text.idFile.slice(8).trim()
+  const evaluations = text.evaluations || {}
+  const numberOfEvaluationsByType = Object.keys(evaluations[type] || {}).length
+  acc[id] = acc[id] || {total: 0, counts: [0, 0, 0, 0, 0, 0, 0]} // Sadly there are texts with more than 2 evaluations. But this is taken into account in the platform
+
+  if (numberOfEvaluationsByType > 2) {
+    debug && console.log(text.idFile, numberOfEvaluationsByType)
+  }
+
+  acc[id].total = acc[id].total + 1
+  acc[id].counts[numberOfEvaluationsByType] += 1
+  return acc
+}, {})
+
+console.log(stats)
